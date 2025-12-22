a0372b637b4ca525c294260128c03c591a49542a
michal <michalkordyzon@gmail.com>
2025-12-22 14:46:35 +0100

    1

--- app/test2.py
+++
@@ -1,5 +0,0 @@
-from milvus_utils import get_wxd_client
-client = get_wxd_client()
-#print([m for m in dir(client) if "pause" in m.lower()])
-print([m for m in dir(client)])
-#print([m for m in dir(client) if "resume" in m.lower()])
